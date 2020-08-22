3c28ba4e374ef117753c6e8765b19a6d49db5d04
yathavan <yathavancom@gmail.com>
2020-08-23 00:42:20 +0530

    pass id via children & psaa via pros deleted

--- simple-counter/src/component/counter.jsx
+++ simple-counter/src/component/counter.jsx
@@ -19,8 +19,6 @@ class Counter extends Component {
 
     return (
       <div className="mb-3">
-        {this.props.children}
-        <h5>title - {this.props.id}</h5>
 
         <span className="py-2 px-4 mr-3 badge badge-primary">
           {count === 0 ? "Zero" : count}
